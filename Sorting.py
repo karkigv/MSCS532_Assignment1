8316904ea797ae3bba577b45d71986b562baefc3
Gaurab Karki <gaurabkarki@heola.lan>
2025-01-12 17:51:15 -0600

    Added Comments

--- Sorting.py
+++ Sorting.py
@@ -1,15 +1,17 @@
 def insertion_sort_descending(arr):
+    # Traverse from the second element to the end
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
 
+        # Move elements of arr[0..i-1] that are smaller than key to one position ahead of their current position
         while j >= 0 and arr[j] < key:
             arr[j + 1] = arr[j]
             j -= 1
 
         arr[j + 1] = key
 
-# Example usage
+# Example usage:
 if __name__ == "__main__":
     array = [12, 11, 13, 5, 6]
     print("Original array:", array)
